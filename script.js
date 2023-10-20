b63b3ce62e35e7b772ecabd48f21c2e03218b178
0D1Um3 <125272910+0D1Um3@users.noreply.github.com>
2023-10-20 15:39:22 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,48 @@
-var prof1 = document.getElementById('prof1').value;
-var prof2 = document.getElementById('prof2').value;
-var value1 = document.getElementById('value1').value;
-var value2 = document.getElementById('value2').value;
-
-
 
 function createObject(prof1, prof2, value1, value2) {
     return {
         [prof1]: value1,
         [prof2]: value2,
+        Print(){
+            return `${prof1}: ${value1}, ${prof2}: ${value2} <br>`;
+        }
     }
 
 }
 
-var Car1 = createObject(prof1, prof2, value1, value2);
 
+var Persons = [
+    new createObject('Имя', 'Возраст', 'Анатолий', 18),
+    new createObject('Имя', 'Возраст', 'Макс', 19),
+    new createObject('Имя', 'Возраст', 'Илья', 19)
+];
 
-function Person(obj) {
+var Cars = [
+    new createObject('Марка', 'ДатаВыпуска', 'Lamborghini Aventador', 2011),
+    new createObject('Марка','ДатаВыпуска', 'Koenigsegg Jesko Absolut', 2020),
+    new createObject('Марка','ДатаВыпуска', 'Toyota AE86', 1983)
+];
 
-    Persons.push(obj);
-
+var Book = [
+    new createObject('НазваниеКниги', 'ДатаПубликации', 'The Witcher. The Last Wish', 1986),
+    new createObject('НазваниеКниги', 'ДатаПубликации', 'The Hobbit', 1937),
+    new createObject('НазваниеКниги', 'ДатаПубликации', 'Дом в котором', 2000)
+];
+document.writeln('Люди' + '<br>');
+for(var i = 0; i < Persons.length; i++){
+    document.writeln(Persons[i].Print() + '<br>');
+}
+document.writeln('Машины' + '<br>');
+for(var i = 0; i < Cars.length; i++){
+    document.writeln(Cars[i].Print() + '<br>');
+}
+document.writeln('Книги' + '<br>');
+for(var i = 0; i < Book .length; i++){
+    document.writeln(Book[i].Print() + '<br>');
 }
 
-var Persons = [];
-var Cars = [];
-var Book = [];
 
-document.writeln(Persons);
+
 
 
 
